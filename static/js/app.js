2f742cbd738c986ca0993512cd2ee0e90187de2a
Jeniear <135406459+Jienniers@users.noreply.github.com>
2024-12-06 23:53:53 +0500

    Improved app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,15 @@
 // Button Action For Video Download Button
-
-function onButtonClick() {
-    alert("Your Video Has Started Download! Please don't close the current tab.")
+function onVideoDownloadClick() {
+    alert("Your Video Has Started Downloading! Please don't close the current tab.");
 }
 
 const videoDownloadButton = document.getElementById('video_download_btn_func');
-
-button.addEventListener('click', onButtonClick);
+videoDownloadButton.addEventListener('click', onVideoDownloadClick);
 
 // Button Action For Audio Download Button
-
-function onButtonClick() {
-    alert("Your Audio Has Started Download! Please don't close the current tab.")
+function onAudioDownloadClick() {
+    alert("Your Audio Has Started Downloading! Please don't close the current tab.");
 }
 
-const AudioDownloadButton = document.getElementById('audio_download_btn_func');
-
-button.addEventListener('click', onButtonClick);
+const audioDownloadButton = document.getElementById('audio_download_btn_func');
+audioDownloadButton.addEventListener('click', onAudioDownloadClick);
