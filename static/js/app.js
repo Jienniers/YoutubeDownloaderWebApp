41e4438d9a53a4b79b8fa3a8f0d75a39c14d892b
Jeniear <135406459+Jienniers@users.noreply.github.com>
2024-12-06 20:39:51 +0500

    Added Alert for Audio Download Button

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,19 @@
+// Button Action For Video Download Button
+
 function onButtonClick() {
     alert("Your Video Has Started Download! Please don't close the current tab.")
 }
 
-const button = document.getElementById('download_btn_func');
+const videoDownloadButton = document.getElementById('video_download_btn_func');
+
+button.addEventListener('click', onButtonClick);
+
+// Button Action For Audio Download Button
+
+function onButtonClick() {
+    alert("Your Audio Has Started Download! Please don't close the current tab.")
+}
+
+const AudioDownloadButton = document.getElementById('audio_download_btn_func');
 
 button.addEventListener('click', onButtonClick);
