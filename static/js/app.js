1e3450b21773e388771117fed06aa0b45aa18026
Jeniear <135406459+Jienniers@users.noreply.github.com>
2024-12-07 15:07:41 +0500

    Updated the alert messages in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // Button Action For Video Download Button
 function onVideoDownloadClick() {
-    alert("Your Video Has Started Downloading! Please don't close the current tab.");
+    alert("Your video will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
 }
 
 const videoDownloadButton = document.getElementById('video_download_btn_func');
@@ -8,7 +8,7 @@ videoDownloadButton.addEventListener('click', onVideoDownloadClick);
 
 // Button Action For Audio Download Button
 function onAudioDownloadClick() {
-    alert("Your Audio Has Started Downloading! Please don't close the current tab.");
+    alert("Your audio will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
 }
 
 const audioDownloadButton = document.getElementById('audio_download_btn_func');
