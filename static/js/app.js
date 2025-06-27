39d90849ea16e670106cb05417a2d962c1730dcd
Jeniear <135406459+Jienniers@users.noreply.github.com>
2025-06-27 18:38:15 +0500

    Inline download button alert handlers in app.js
    Replaced separate named functions for video and audio download button alerts with inline anonymous functions to simplify the code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,13 @@
 // Button Action For Video Download Button
-function onVideoDownloadClick() {
-    alert("Your video will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
-}
-
 const videoDownloadButton = document.getElementById('video_download_btn_func');
-videoDownloadButton.addEventListener('click', onVideoDownloadClick);
+
+videoDownloadButton.addEventListener('click', function () {
+    alert("Your video will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
+});
 
 // Button Action For Audio Download Button
-function onAudioDownloadClick() {
-    alert("Your audio will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
-}
-
 const audioDownloadButton = document.getElementById('audio_download_btn_func');
-audioDownloadButton.addEventListener('click', onAudioDownloadClick);
+
+audioDownloadButton.addEventListener('click', function () {
+    alert("Your audio will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
+});
