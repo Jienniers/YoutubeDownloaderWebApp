76d28580e22c032a91919dec1d51f4ed091c90c4
Jeniear <135406459+Jienniers@users.noreply.github.com>
2025-06-28 06:02:38 +0500

    Fix download button IDs and center video title
    Updated the IDs for audio and video download buttons in index.html and app.js to ensure correct event handling. Also centered the video title for improved UI presentation.

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,12 @@
 // Button Action For Video Download Button
-const videoDownloadButton = document.getElementById('video_download_btn_func');
+const videoDownloadButton = document.getElementById('video_download_btn');
 
 videoDownloadButton.addEventListener('click', function () {
     alert("Your video will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
 });
 
 // Button Action For Audio Download Button
-const audioDownloadButton = document.getElementById('audio_download_btn_func');
+const audioDownloadButton = document.getElementById('audio_download_btn');
 
 audioDownloadButton.addEventListener('click', function () {
     alert("Your audio will start downloading once you click the Ok button on this alert. Please don't close the current tab.");
